008fd5b5ce2eb55b1a9658d204be5d8c1dcfc62f
MUYANG GUO <muyangguo@gmail.com>
2020-01-23 15:32:46 -0500

    update

---
+++ My_workspace/host/000TestProject/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 23, 2020
+ *      Author: muyangguo
+ */
+
+
+#include<stdio.h>
+
+int main(void){
+	printf("Hello World\n");
+	printf("Press 'Enter' to exit this application\n");
+	getchar();
+	return 0;
+}
